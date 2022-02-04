13f5c1663bdbff83c7bab7d3b7e832d955a95d68
Tony <antoniu.tudor@trip-express.ro>
2022-02-04 09:33:10 +0200

    flyway db - add script

--- src/main/java/Utils/JdbcConnection.java
+++ src/main/java/Utils/JdbcConnection.java
@@ -1,59 +1,32 @@
 package Utils;
 
-import java.sql.*;
-import java.time.LocalDate;
+import com.bookxchange.Model.Author;
+import org.flywaydb.core.Flyway;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
+
 
 public class JdbcConnection {
 
-    static final String DB_URL = "jdbc:mysql://localhost:3306/bookxchange";
-    static final String USER = "root";
-    static final String PASS = "root";
-
-
-
-
-    private static final String insertBook =  "INSERT INTO books(isbn, title, author, description , sellAvailability,rentAvailability, quantity)"
-        + " VALUES('1234','harap alb', '1', ' o carte frumoasa', true, false, 2)";
-
-    private static final String insertAuthor = "INSERT INTO Authors(id,name, surname, birthDate)"
-        + " VALUES('1','andrei','popescu','2022-02-01')";
-
-    private static final String getSql = "SELECT * FROM Authors";
-
     public static void main(String[] args) {
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(DB_URL,USER,PASS);
-            System.out.println("Got it!");
+        String url = "jdbc:mysql://localhost:3306/";
+        String user = "root";
+        String pass = "root";
 
-            // create a Statement from the connection
-          Statement statement = conn.createStatement();
-//
-//            statement.executeUpdate(insertBook);
+        File resourcesDirectory = new File("src/main/resources");
+        String absolutePath = resourcesDirectory.getAbsolutePath();
 
+        Flyway fdb = Flyway.configure()
+                .createSchemas(true)
+                .dataSource(url,user,pass)
+                .locations("filesystem:"+absolutePath+"/db/migration")
+                .schemas("bookOLX")
+                .load();
 
-            ResultSet result  = statement.executeQuery(getSql);
-
-            while(result.next()) {
-                System.out.println(result.getInt(1));
-                System.out.println(result.getString(2));
-                System.out.println(result.getString(3));
-                System.out.println(result.getString(4));
-            }
-
-
-        } catch (SQLException e) {
-            throw new Error("Problem", e);
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
+        fdb.clean();
+        fdb.migrate();
     }
-
-
 }
---
+++ src/main/resources/db/migration/V1__init_db_table.sql
@@ -0,0 +1,49 @@
+BEGIN;
+CREATE DATABASE IF NOT EXISTS bookOLX ;
+
+USE bookOLX;
+
+
+CREATE TABLE Authors (
+                         id varchar(36) NOT NULL unique,
+                         name VARCHAR(25),
+                         surname VARCHAR (25),
+                         birthDate DATE,
+                         PRIMARY KEY (id)
+);
+
+
+CREATE TABLE books (
+                       isbn varchar(20) NOT NULL UNIQUE,
+                       title VARCHAR(100),
+                       author varchar(36),
+                       description TEXT,
+                       quantity INT,
+                       PRIMARY KEY (isbn),
+                       FOREIGN KEY (author) REFERENCES Authors(id)
+);
+
+
+
+CREATE TABLE Members (
+                         userID varchar(36) NOT NULL,
+                         username TEXT,
+                         primary key (userID)
+);
+
+
+CREATE TABLE BookMarket (
+                            id VARCHAR(36) not null unique,
+                            userID VARCHAR(36) not null,
+                            bookID VARCHAR(36) not null,
+                            bookState TEXT,
+                            forSell boolean,
+                            sellPrice double,
+                            forRent boolean,
+                            rentPrice double,
+                            bookStatus text,
+                            primary key(id),
+                            foreign key (userID) references Members(userID),
+                            foreign key (bookID) references books(isbn)
+);
+
