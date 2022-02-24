6f59136ca408eeabb792207f3663644fabb2cedb
MihaiC-VDF <mihaicucuianu@gmail.com>
2022-02-24 16:03:10 +0200

    Book service functionality for adding a book complete

--- src/main/java/com/bookxchange/service/BookService.java
+++ src/main/java/com/bookxchange/service/BookService.java
@@ -2,6 +2,7 @@ package com.bookxchange.service;
 
 import com.bookxchange.customExceptions.InvalidISBNException;
 import com.bookxchange.dto.BookDto;
+import com.bookxchange.enums.BookState;
 import com.bookxchange.model.Author;
 import com.bookxchange.model.Book;
 import com.bookxchange.repositories.BookRepo;
@@ -20,7 +21,7 @@ import java.util.UUID;
 public class BookService {
 
 
-    public static void main(String[] args) throws SQLException, IOException {
+    public static void main(String[] args) throws SQLException {
 
 
         BookRepo bookRepo = new BookRepo();
@@ -32,10 +33,42 @@ public class BookService {
     }
 
 
-    public void addBookToDB(String isbn, boolean rent, double rentPrice, boolean sell, double sellPrice, long userID) throws SQLException {
-        IsbnService isbnChecker = new IsbnService();
+    public void userAddsNewBook(String providedIsbn, boolean rent, double priceToRent, boolean sell, double priceToSell, long recievedUserID, BookState providedBookState) throws SQLException {
+        addingBookToDataBase(providedIsbn);
+        addingMarketBookForUser(providedIsbn, rent, priceToRent, sell, priceToSell, recievedUserID, providedBookState);
+    }
 
-//        Check If Book is in database.
+
+    private void addingBookToDataBase(String isbn) throws SQLException {
+
+//    If a book is present in the DB then this service will not do any process
+        if (!checkIfBookIsInDataBase(isbn)) {
+
+//            Contacts the ISBN service to retrieve book details
+            IsbnService retrivedBook = new IsbnService();
+
+//            Checks and adds Authors from the retrievedBook, if they exist in DB updates them with existing UUID to creat the m2m entry between Book and Author(s)
+            Book retrivedBookDetailsFromExternal = retrivedBook.hitIsbnBookRequest(isbn);
+            Iterator<Author> authorList = retrivedBookDetailsFromExternal.getAuthors().listIterator();
+            while (authorList.hasNext()) {
+                checkAuthorUpdateOrAddDB(authorList.next());
+            }
+
+//            Inserting new book in to the database
+            insertBookInDataBase(retrivedBookDetailsFromExternal);
+
+//            Insert m2m entry for book and authors(retrivedBookDetailsFromExternal, authorList)
+            insertBookToAuthorsDBEntry(isbn, retrivedBookDetailsFromExternal.getAuthors());
+
+
+        }
+
+    }
+
+
+    //    Checks if a book is present in the database
+    private boolean checkIfBookIsInDataBase(String isbn) throws SQLException {
+
         String sqlSeeIfBookIsPresent = "SELECT\n" +
                 "books.isbn\n" +
                 "FROM\n" +
@@ -45,91 +78,40 @@ public class BookService {
 
         try (Connection con = JdbcConnection.getConnection();
              PreparedStatement ps = con.prepareStatement(sqlSeeIfBookIsPresent)) {
+
+//            if present will execute method to update quantity by 1
             try (ResultSet rs = ps.executeQuery()) {
                 if (rs.isBeforeFirst()) {
-                    addMarketBookOnly(isbn, rent, rentPrice, sell, sellPrice, userID);
+                    updateQuantityForExistingBook(isbn);
+                    return true;
                 } else {
-//                    addBookAndMarketBook();
+                    return false;
                 }
             }
         }
+    }
 
-//        If present add just user entry and update quantity
-        String sqlRetrievedQuantity = "SELECT\n" +
-                "books.quantity\n" +
-                "FROM\n" +
+
+    //    If called method updates book quantity by 1
+    private void updateQuantityForExistingBook(String isbn) throws SQLException {
+
+        String sqlUpdateQuantity = "UPDATE\n" +
                 "books\n" +
+                "SET\n" +
+                "quantity = quantity+1\n" +
                 "WHERE\n" +
                 "books.isbn = isbn";
 
-
-    }
-
-    public void addBookAndMarketBook(String isbn, boolean rent, double rentPrice, boolean sell, double sellPrice, long userID) throws SQLException {
-
-//        Check if author is present in database.
-
-
-//        add book
-        IsbnService isbnBookRetriever = new IsbnService();
-        Book bookToAdd = isbnBookRetriever.hitIsbnBookRequest(isbn);
-        if(bookToAdd.equals(null)){
-            throw new InvalidISBNException(isbn + " is invalid");
-        } else {
-            addingBookToDataBase(bookToAdd);
-          ListIterator<Author> iterateAuthors = bookToAdd.getAuthors().listIterator();
-
-          while (iterateAuthors.hasNext()) {
-              checkAuthorUpdateOrAddDB(iterateAuthors.next());
-          }
-
-
-
-
-        }
-
-//        add author/s
-
-//        add user enry
-
-    }
-
-    public void addMarketBookOnly(String isbn, boolean rent, double rentPrice, boolean sell, double sellPrice, long userID) {
-
-    }
-
-    public void addingBookToDataBase(Book addedBook) throws SQLException{
-        String sqlForInsertingNewBook = "INSERT INTO\n" +
-                "books\n" +
-                "(isbn,title,description,quantity)\n" +
-                "VALUE(addedBook.getIsbn(),addedBook.getTitle(),addedBook.getDescription(),1);\n";
-
         try (Connection con = JdbcConnection.getConnection();
-             PreparedStatement ps = con.prepareStatement(sqlForInsertingNewBook)) {
+             PreparedStatement ps = con.prepareStatement(sqlUpdateQuantity)) {
             ps.executeQuery();
         }
     }
 
-    private void addAuthor(Author authorToAdd) throws SQLException{
-        UUID authorIDToAdd = authorToAdd.getId();
-        String authorNameToAdd = authorToAdd.getName();
-        String authorSurnameToAdd = authorToAdd.getSurname();
 
-
-        String sqlForInsertingNewAuthor = "INSERT INTO" +
-                "Authors" +
-                "(id, name, surname)" +
-                "(authorIDToAdd, authorToAdd.getName(), authorToAdd.getSurname())";
-
-        try (Connection con = JdbcConnection.getConnection();
-             PreparedStatement ps = con.prepareStatement(sqlForInsertingNewAuthor)) {
-             ps.executeQuery();
-        }
-
-    }
-
-
-
+    //    Veryfies checked author if is present in the database by matching both name and surname
+//    On match updates the checked author id from the DB to be used in the users book entry flow
+//    If not found sets a UUID and adds given author to the database
     private void checkAuthorUpdateOrAddDB(Author checkedAuthor) throws SQLException {
 
         String sqlCheckAuthorExistsUpdateUUID = "SELECT\n" +
@@ -144,8 +126,8 @@ public class BookService {
              PreparedStatement ps = con.prepareStatement(sqlCheckAuthorExistsUpdateUUID)) {
             try (ResultSet rs = ps.executeQuery()) {
                 if (rs.isBeforeFirst()) {
-                        checkedAuthor.setId(UUID.fromString(rs.getString("Authors.id")));
-                    } else {
+                    checkedAuthor.setId(UUID.fromString(rs.getString("Authors.id")));
+                } else {
                     checkedAuthor.setId(UUID.randomUUID());
                     addAuthor(checkedAuthor);
                 }
@@ -154,6 +136,67 @@ public class BookService {
     }
 
 
+    //    Method adds provided author to the database
+    private void addAuthor(Author authorToAdd) throws SQLException {
+
+        String sqlForInsertingNewAuthor = "INSERT INTO" +
+                "Authors" +
+                "(id, name, surname)" +
+                "VALUES(authorToAdd.getId(), authorToAdd.getName(), authorToAdd.getSurname())";
+
+        try (Connection con = JdbcConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sqlForInsertingNewAuthor)) {
+            ps.executeQuery();
+        }
+
+    }
+
+
+    private void insertBookInDataBase(Book bookToInsert) throws SQLException {
+
+//        bookToInsert.getIsbn(), bookToInsert.getTitle(), bookToInsert.getDescription()
+        String sqlForInsertingNewBook = "INSERT INTO\n" +
+                "books\n" +
+                "(isbn,title,description,quantity)\n" +
+                "VALUES(bookToInsert.get(),addedBook.getTitle(),addedBook.getDescription(),1);\n";
+
+        try (Connection con = JdbcConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sqlForInsertingNewBook)) {
+            ps.executeQuery();
+        }
+    }
+
+
+    private void insertBookToAuthorsDBEntry(String isbn, List<Author> bookAuthors) throws SQLException {
+
+        for (int i = 0; i < bookAuthors.size(); i++) {
+
+            String sqlQueryForLinkingBooksToAuthorsEntry = "INSERT INTO\n" +
+                    "author_book_mtm\n" +
+                    "(id, book_isbn, author_id)\n" +
+                    "VALUES(UUID.randomUUID(), isbn, bookAuthors.get(i).getId())";
+
+            try (Connection con = JdbcConnection.getConnection();
+                 PreparedStatement ps = con.prepareStatement(sqlQueryForLinkingBooksToAuthorsEntry)) {
+                ps.executeQuery();
+            }
+        }
+    }
+
+
+    //    Adds BookMarket entry in DB
+    private void addingMarketBookForUser(String providedIsbn, boolean rent, double priceToRent, boolean sell, double priceToSell, long recievedUserID, BookState providedBookState) throws SQLException {
+
+        String sqlQueryForBookMarketEntry = "INSERT INTO\n" +
+                "author_book_mtm\n" +
+                "(id, userID, bookID, bookState, forSell, sellPrice, forRent, rentPrice, bookStatus)\n" +
+                "VALUES(UUID.randomUUID(), recievedUserID, providedIsbn, providedBookState, sell, priceToSell, rent, priceToRent, AVAILABLE)";
+
+        try (Connection con = JdbcConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sqlQueryForBookMarketEntry)) {
+            ps.executeQuery();
+        }
+    }
 
 
 }
