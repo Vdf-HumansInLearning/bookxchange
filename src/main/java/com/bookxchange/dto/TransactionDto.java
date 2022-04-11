49856aab704d6808f2c7668ab2b32dcfa56e560b
Tony <antoniu.tudor@trip-express.ro>
2022-04-11 17:41:39 +0300

    - repaired tests

--- src/main/java/com/bookxchange/dto/TransactionDto.java
+++ src/main/java/com/bookxchange/dto/TransactionDto.java
@@ -9,13 +9,9 @@ import javax.validation.constraints.NotBlank;
 @Setter
 public class TransactionDto {
 
-    @NotBlank(message = "marketBookId is mandatory")
     private String marketBookId;
-    @NotBlank(message = "supplier is mandatory")
     private String supplier;
-    @NotBlank(message = "client is mandatory")
     private String client;
-    @NotBlank(message = "transactionType is mandatory")
     private TransactionType transactionType;
 
 }
---
+++ src/test/java/TransactionsTest/TransactionsTests.java
@@ -0,0 +1,5 @@
+package TransactionsTest;
+
+public class TransactionsTests {
+
+}
